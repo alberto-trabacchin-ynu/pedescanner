1516384cae226d1e9bacc4eb46d9bebc50c25b40
Alberto Trabacchin <alberto-trabacchin-wj@ynu.jp>
2022-12-28 19:14:52 +0900

    Reading vehicle information now allows to select only some columns.

--- pedescanner/jari_mng/test.py
+++ pedescanner/jari_mng/test.py
@@ -1,12 +1,15 @@
 import unittest
 import pedescanner.jari_mng.vehicle_info as vehicle_info
 
+record_name = "03_20160119_174003"
+
 class TestJariMethods(unittest.TestCase):
     
     def test_read_vehicle_data(self):
-        df = vehicle_info.read_vehicle_data("03_20160119_174003")
-        n_features = df.shape[1]
-        self.assertEqual(n_features, 9)
+        df = vehicle_info.read_vehicle_data(record_name)
+        self.assertEqual(df.shape[1], 9)
+        df = vehicle_info.read_vehicle_data(record_name, sel_fields=["time", "x_accel"])
+        self.assertEqual(df.shape[1], 2)
 
 if __name__ == "__main__":
     unittest.main()
--- pedescanner/jari_mng/vehicle_info.py
+++ pedescanner/jari_mng/vehicle_info.py
@@ -2,10 +2,16 @@ from pathlib import Path
 import os
 import pandas as pd
 
-def read_vehicle_data(record_name: str) -> None:
+def get_full_path(record_name: str):
     full_path = Path().absolute() / "datasets" / "jari" / record_name / "車両情報"
     full_path = list(full_path.iterdir())[0]
-    df = pd.read_csv(full_path, encoding="cp932")
+    return full_path
+
+def read_vehicle_data(record_name: str, sel_fields=None) -> pd.DataFrame:
+    field_names = ["time", "x_vel", "x_accel", "brake_light", "yaw_vel",
+                  "y_accel", "steering_ang", "accel_pedal", "brake_pedal"]
+    df = pd.read_csv(get_full_path(record_name), header=None, skiprows=1, 
+                     names=field_names, usecols=sel_fields, encoding="cp932")
     return df
 
 if __name__ == "__main__":
