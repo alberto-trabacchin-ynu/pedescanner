8c993e5a93f58f358da012b0f56a429e8d137a39
Alberto Trabacchin <alberto-trabacchin-wj@ynu.jp>
2022-12-29 10:38:41 +0900

    Shortened code for reading dataset tracking.

--- pedescanner/jari_mng/test.py
+++ pedescanner/jari_mng/test.py
@@ -11,5 +11,9 @@ class TestJariMethods(unittest.TestCase):
         df = vehicle_info.read_vehicle_data(record_name, sel_fields=["time", "x_accel"])
         self.assertEqual(df.shape[1], 2)
 
+    def test_plot_data(self):
+        df = vehicle_info.read_vehicle_data(record_name)
+        vehicle_info.plot_data(df.head(2000), x_field="time", y_field=["x_vel", "accel_pedal", "brake_pedal"])
+
 if __name__ == "__main__":
     unittest.main()
--- pedescanner/jari_mng/vehicle_info.py
+++ pedescanner/jari_mng/vehicle_info.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 def get_full_path(record_name: str):
     full_path = Path().absolute() / "datasets" / "jari" / record_name / "車両情報"
@@ -10,9 +12,26 @@ def get_full_path(record_name: str):
 def read_vehicle_data(record_name: str, sel_fields=None) -> pd.DataFrame:
     field_names = ["time", "x_vel", "x_accel", "brake_light", "yaw_vel",
                   "y_accel", "steering_ang", "accel_pedal", "brake_pedal"]
-    df = pd.read_csv(get_full_path(record_name), header=None, skiprows=1, 
+    df = pd.read_csv(get_full_path(record_name), header=None, skiprows=2, 
                      names=field_names, usecols=sel_fields, encoding="cp932")
+    #df = df.replace(r"^\s*$", np.nan, regex=True)
     return df
+
+def plot_data(df: pd.DataFrame, x_field: str, y_field: str):
+    x_vals = df[x_field].to_numpy()
+    x_vals = 0.1 * np.arange(0, 2000, 1)
+    y1_vals = df[y_field[0]].to_numpy(dtype=float)
+    y1_vals = y1_vals / np.max(y1_vals)
+    y2_vals = df[y_field[1]].to_numpy(dtype=float)
+    y2_vals = y2_vals / 5
+    y3_vals = df[y_field[2]].to_numpy(dtype=float)
+    y3_vals = y3_vals / 5
+    plt.plot(x_vals, y1_vals)
+    plt.plot(x_vals, y2_vals)
+    plt.plot(x_vals, y3_vals)
+    plt.xlabel(x_field)
+    plt.ylabel(y_field)
+    plt.show()
 
 if __name__ == "__main__":
     pass
